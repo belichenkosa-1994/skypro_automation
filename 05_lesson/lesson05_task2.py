34addb65d820a54afc224778a51ba4efeaa36409
belichenkosa-1994 <belichenkosa.1994@gmail.com>
2025-12-01 14:35:58 +0300

    lesson5-2

--- 05_lesson/lesson05_task2.py
+++ 05_lesson/lesson05_task2.py
@@ -1,14 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 
-driver = webdriver.Firefox()
-driver.get("http://uitestingplayground.com/dynamicid")
+driver = webdriver.Chrome()
+driver.get("http://uitestingplayground.com/classattr")
 
-blue_button = driver.find_element(
-    By.CSS_SELECTOR, "button[class*='btn-primary']"
-)
+blue_button = driver.find_element(By.CSS_SELECTOR, ".btn-primary")
 blue_button.click()
 
-time.sleep(2)
 driver.quit()
